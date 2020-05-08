6e8e56a75747f3475dbead646e10c15c0bf11b62
drinaa <46159789+drinaa@users.noreply.github.com>
2020-05-08 16:03:48 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.But_cnt = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox_ResultMK = new System.Windows.Forms.TextBox();
+            this.textBox_ResultSrandart = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.But_cnt.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel3.Controls.Add(this.But_cnt, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1067, 610);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // But_cnt
+            // 
+            this.But_cnt.ColumnCount = 2;
+            this.But_cnt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.But_cnt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.But_cnt.Controls.Add(this.textBox_ResultSrandart, 1, 4);
+            this.But_cnt.Controls.Add(this.textBox_ResultMK, 1, 3);
+            this.But_cnt.Controls.Add(this.textBox8, 1, 2);
+            this.But_cnt.Controls.Add(this.textBox7, 1, 1);
+            this.But_cnt.Controls.Add(this.textBox5, 0, 4);
+            this.But_cnt.Controls.Add(this.textBox4, 0, 3);
+            this.But_cnt.Controls.Add(this.textBox3, 0, 2);
+            this.But_cnt.Controls.Add(this.textBox1, 0, 1);
+            this.But_cnt.Controls.Add(this.textBox2, 0, 0);
+            this.But_cnt.Controls.Add(this.button1, 1, 5);
+            this.But_cnt.Controls.Add(this.textBox6, 1, 0);
+            this.But_cnt.Location = new System.Drawing.Point(3, 3);
+            this.But_cnt.Name = "But_cnt";
+            this.But_cnt.RowCount = 6;
+            this.But_cnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.But_cnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.But_cnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.But_cnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.But_cnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.But_cnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.But_cnt.Size = new System.Drawing.Size(367, 604);
+            this.But_cnt.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox5.Location = new System.Drawing.Point(4, 405);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(212, 77);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "Площадь круга стандартным способом (pi*r^2):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox4.Location = new System.Drawing.Point(4, 305);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(212, 54);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Площадь круга: метод Монте-Карло";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox3.Location = new System.Drawing.Point(4, 205);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(212, 54);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "Скорость заполения в мс:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(4, 105);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(212, 54);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Количество потоков:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox2.Location = new System.Drawing.Point(4, 5);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(212, 54);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Радиус круга в px:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(223, 537);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 30);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox6.Location = new System.Drawing.Point(223, 3);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(141, 26);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox7.Location = new System.Drawing.Point(223, 103);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(141, 26);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox8.Location = new System.Drawing.Point(223, 203);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(141, 26);
+            this.textBox8.TabIndex = 15;
+            // 
+            // textBox_ResultMK
+            // 
+            this.textBox_ResultMK.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_ResultMK.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox_ResultMK.Location = new System.Drawing.Point(224, 305);
+            this.textBox_ResultMK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_ResultMK.Name = "textBox_ResultMK";
+            this.textBox_ResultMK.ReadOnly = true;
+            this.textBox_ResultMK.Size = new System.Drawing.Size(139, 19);
+            this.textBox_ResultMK.TabIndex = 16;
+            // 
+            // textBox_ResultSrandart
+            // 
+            this.textBox_ResultSrandart.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_ResultSrandart.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox_ResultSrandart.Location = new System.Drawing.Point(224, 405);
+            this.textBox_ResultSrandart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox_ResultSrandart.Name = "textBox_ResultSrandart";
+            this.textBox_ResultSrandart.ReadOnly = true;
+            this.textBox_ResultSrandart.Size = new System.Drawing.Size(139, 19);
+            this.textBox_ResultSrandart.TabIndex = 17;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(376, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(688, 604);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 608);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.But_cnt.ResumeLayout(false);
+            this.But_cnt.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel But_cnt;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox_ResultSrandart;
+        private System.Windows.Forms.TextBox textBox_ResultMK;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        int Radius;
+        int QuantThreads;
+        int Speed;
+        int X;//левый верхний угол квадрата
+        int Y;
+        double res = 0;//используя метод Монте-Карло
+        double StdRes = 0;//используя стандартную формулу
+        int PointRect = 0;//количество точек в квадрате
+        int PointCrc = 0;//количество точек в круге
+        List<Color> Colors = new List<Color>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Check())
+            {
+                StdRes = Math.Round((Math.PI * Radius * Radius), 0);
+                res = 0;
+                textBox_ResultSrandart.Text = StdRes.ToString();
+                Colors.Clear();
+                Painting();
+                Async_tasks();
+            }
+        }
+        public async Task Async_tasks()
+        {
+            Task[] tasks = new Task[QuantThreads];
+
+            Random rand = new Random();
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                Colors.Add(Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256)));//задание цвета потокам
+                tasks[i] = await Task.Run(async () => Work(i));
+            }
+        }
+
+        public async Task Work(int i)
+        {
+            Random rnd = new Random();
+
+            while (Math.Abs(res - StdRes) >= 20)
+            {
+                int x = rnd.Next(X, X + 2 * Radius);//координаты точки в квадрате
+                int y = rnd.Next(Y, Y + 2 * Radius);
+
+                //проверка попадания точки в круг
+                if (((X + Radius) - x) * ((X + Radius) - x) + ((Y + Radius) - y) * ((Y + Radius) - y) <= Radius * Radius)
+                {
+                    Interlocked.Increment(ref PointCrc);
+                }
+                Interlocked.Increment(ref PointRect);
+
+                //отображение точки
+                Brush b = new SolidBrush(Colors[i]);
+                Point(x, y, b);
+
+                //вычисление по методу МК
+                int localres = (4 * Radius * Radius * PointCrc/ PointRect);
+                Interlocked.Exchange(ref res, localres);
+
+                textBox_ResultMK.Invoke(new Action(() => textBox_ResultMK.Text = res.ToString()));
+                await Task.Delay(Speed);
+            }
+        }
+
+        /// <summary>
+        /// Отрисовка квадрата и круга
+        /// </summary>
+        private void Painting()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(pictureBox1.BackColor);
+
+            X = (pictureBox1.Right - pictureBox1.Left) / 2 - Radius;
+            Y = (pictureBox1.Bottom - pictureBox1.Top) / 2 - Radius;
+
+            Rectangle r = new Rectangle(X, Y, 2 * Radius, 2 * Radius);
+            Brush b = new SolidBrush(Color.Violet);
+            Pen p = new Pen(b, 3);
+            g.DrawRectangle(p, r);
+
+            b = new SolidBrush(Color.Yellow);
+            p = new Pen(b, 3);
+            g.DrawEllipse(p, r);
+        }
+
+        /// <summary>
+        /// Отрисовка точки
+        /// </summary>
+        /// <param name="x">координата</param>
+        /// <param name="y">координата</param>
+        /// <param name="b">цвет</param>
+        private void Point(int x, int y, Brush b)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            Pen p = new Pen(b);
+            Rectangle point = new Rectangle(x, y, 3, 3);
+            g.DrawRectangle(p, point);
+            g.FillRectangle(b, point);
+        }
+
+        /// <summary>
+        /// Проверка заполненных полей
+        /// </summary>
+        /// <returns></returns>
+        public bool Check()
+        {
+            bool res = true;
+            if (CheckInt(textBox6.Text))
+            {
+                Int32.TryParse(textBox6.Text, out Radius);
+                if (Radius * 2 > pictureBox1.Width || Radius * 2 > pictureBox1.Height)
+                {
+                    MessageBox.Show("Введите, пожалуйста, радиус меньше!");
+                    return false;
+                }
+                if (Radius<40)
+                {
+                    MessageBox.Show("Введите, пожалуйста, радиус больше!");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Некорректный радиус!");
+                return false;
+            }
+            if (CheckInt(textBox7.Text))
+            {
+                Int32.TryParse(textBox7.Text, out QuantThreads);
+            }
+            else
+            {
+                MessageBox.Show("Некорректное число потоков!");
+                return false;
+            }
+            if (CheckInt(textBox8.Text))
+            {
+                Int32.TryParse(textBox8.Text, out Speed);
+            }
+            else
+            {
+                MessageBox.Show("Некорректная скорость!");
+                return false;
+            }
+            return res;
+        }
+
+        //Число больше 0
+        public bool CheckInt(string s)
+        {
+            int n;
+            if (Int32.TryParse(textBox6.Text, out n))
+            {
+                if (n >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
+
